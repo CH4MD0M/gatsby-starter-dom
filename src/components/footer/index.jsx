ca9b06e779df64c5c6bbbbf664194efb1ef19bb2
KiHoon Roh <started677@gmail.com>
2022-08-03 20:41:51 +0900

    Footer 수정

--- src/components/footer/index.jsx
+++ src/components/footer/index.jsx
@@ -1,17 +1,11 @@
 import React from "react";
-import SocialLinks from "../../constants/SocialLinks";
+
 import "./style.scss";
 
 const Footer = () => {
   return (
     <footer className="footer">
-      <div>
-        <SocialLinks styleClass="footer-icons" />
-        <p>
-          &copy;{new Date().getFullYear()} <a>ch4md0m</a>. Built with{" "}
-          <a>Gatsby-starter-blog</a>
-        </p>
-      </div>
+      <p>&copy; {new Date().getFullYear()}, ch4md0m All rights reserved.</p>
     </footer>
   );
 };
