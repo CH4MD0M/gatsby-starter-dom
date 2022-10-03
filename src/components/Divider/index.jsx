e321036416320b2e7c384806f66fc699e9fb8232
KiHoon Roh <started677@gmail.com>
2022-10-03 23:27:10 +0900

    [feat]Divider 컴포넌트 추가

---
+++ src/components/Divider/index.jsx
@@ -0,0 +1,21 @@
+import PropTypes from "prop-types";
+import styled from "styled-components";
+
+const Divider = styled.hr`
+  margin-top: ${(props) => props.mt};
+  margin-bottom: ${(props) => props.mb};
+  border: none;
+  border-bottom: 1px solid var(--divider);
+`;
+
+Divider.propTypes = {
+  mt: PropTypes.string,
+  mb: PropTypes.string,
+};
+
+Divider.defaultProps = {
+  mt: "2rem",
+  mb: "2rem",
+};
+
+export default Divider;
