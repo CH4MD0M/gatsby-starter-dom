071d79f3c51420dc26075bc0fce445af2c6a80a3
KiHoon Roh <started677@gmail.com>
2022-10-07 21:51:05 +0900

    [feat]PostNavigator 추가

---
+++ src/components/PostNavigator/index.jsx
@@ -0,0 +1,28 @@
+import React from "react";
+import { Link } from "gatsby";
+
+// CSS
+import * as S from "./style";
+
+const PostCard = ({ right, node }) => {
+  const { slug, title } = node?.frontmatter;
+
+  return (
+    <S.PostCardWrapper>
+      <Link to={`/content/${slug}`}>{title}</Link>
+    </S.PostCardWrapper>
+  );
+};
+
+const PostNavigator = ({ pageContext }) => {
+  const { previous, next } = pageContext;
+
+  return (
+    <S.NavigatorWrapper>
+      {previous ? <PostCard node={previous} /> : <div></div>}
+      {next && <PostCard right node={next} />}
+    </S.NavigatorWrapper>
+  );
+};
+
+export default PostNavigator;
---
+++ src/components/PostNavigator/style.js
@@ -0,0 +1,37 @@
+import styled from "styled-components";
+
+export const NavigatorWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  margin: 5rem 0;
+
+  @media screen and (max-width: ${(props) => props.theme.responsive.medium}) {
+    margin-bottom: 80px;
+    padding: 0 12.8px;
+    flex-direction: column;
+
+    & > div:first-child {
+      margin-bottom: 12.8px;
+    }
+  }
+`;
+
+export const PostCardWrapper = styled.div`
+  background: var(--postNavButtonBackground);
+  max-width: 30rem;
+  flex-basis: 30rem;
+  border-radius: 8px;
+  padding: 2rem;
+  white-space: nowrap;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  a {
+    color: var(--textColor);
+    font-weight: 500;
+  }
+  transition: background 0.2s;
+
+  &:hover {
+    background: var(--hoveredPostNavButtonBackground);
+  }
+`;
