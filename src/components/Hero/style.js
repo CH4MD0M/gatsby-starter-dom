59a991323550cf283d54150ff1acad8c3fb8577e
KiHoon Roh <started677@gmail.com>
2022-10-20 22:50:17 +0900

    [feat]Hero 컴포넌트 구현
    - typewriter-effect 라이브러리 설치

---
+++ src/components/Hero/style.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+
+export const HeroWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin: 14rem 0 8rem;
+
+  .Typewriter {
+    min-height: 16rem;
+    font-size: 3.5rem;
+    font-weight: 200;
+    width: 50rem;
+    flex-basis: 50rem;
+  }
+
+  @media screen and (max-width: ${(props) => props.theme.responsive.sm}) {
+    margin: 14rem 0 6rem;
+    .Typewriter {
+      font-size: 2.8rem;
+    }
+  }
+`;
+
+export const Profile = styled.div`
+  display: block;
+  width: 256px;
+  height: 256px;
+  background-image: url(${(props) => props.profileImageRoot}/profile.gif);
+  background-position: center;
+  background-size: contain;
+  background-repeat: no-repeat;
+  border-radius: 999px;
+
+  @media screen and (max-width: ${(props) => props.theme.responsive.sm}) {
+    display: none;
+  }
+`;
