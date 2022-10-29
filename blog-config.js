91ca08226cfadd26054bf5ffde2db79f45b26cb6
KiHoon Roh <started677@gmail.com>
2022-10-29 13:57:31 +0900

    [chore]설정 파일 수정

--- blog-config.js
+++ blog-config.js
@@ -1,18 +1,18 @@
 module.exports = {
   siteUrl: `https://ch4md0m.blog`,
-  title: `ch4md0m.blog`,
+  title: `gatsby-starter-dom`,
   description: `기록하는 프론트엔드 개발자`,
   comments: {
     utterances: {
-      repo: `CH4MD0M/ch4md0m.blog`,
+      repo: `CH4MD0M/gatsby-starter-dom`,
     },
   },
-  ga: "G-2QPXMQNDX9", // Google Analytics Tracking ID
+  ga: "0", // Google Analytics Tracking ID
   author: `Kihoon`,
   name: `노기훈`,
   socials: {
-    email: "d0m__@naver.com",
+    email: "mailto:d0m__@naver.com",
     github: "https://github.com/CH4MD0M/",
-    instagram: `https://www.instagram.com/k1h00n_`,
+    instagram: `https://www.instagram.com/`,
   },
 };
