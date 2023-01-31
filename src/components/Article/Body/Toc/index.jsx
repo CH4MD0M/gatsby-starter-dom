896ca23248bca52d1f752eed082c5c95383fc560
KiHoon Roh <started677@gmail.com>
2023-01-31 13:08:22 +0900

    [refact] TOC 기능 리팩토링

--- src/components/Article/Body/Toc/index.jsx
+++ src/components/Article/Body/Toc/index.jsx
@@ -1,43 +1,50 @@
-import React, { useState, useCallback } from 'react';
+import React, { useState, useCallback, useEffect } from 'react';
 import { animateScroll } from 'react-scroll';
 
 import getElementOffsetY from '../../../../utils/getOffset';
-import useScroll from '../../../../hooks/useScroll';
+import useIntersectionObserver from '../../../../hooks/useIntersectionObserver';
 
 // CSS
 import * as S from './style';
 
 const Toc = () => {
-  const [tocWrapperTop, setTocWrapperTop] = useState();
   const [currentId, setCurrentId] = useState();
   const [headings, setHeading] = useState([]);
-  const scrollY = useScroll();
+  useIntersectionObserver(setCurrentId);
 
+  // TOC-Item Click Handler
   const handleClickHeading = useCallback(itemId => {
     const node = document.getElementById(itemId);
-    animateScroll.scrollTo(getElementOffsetY(node) - 100);
+    animateScroll.scrollTo(getElementOffsetY(node) - 60);
+  }, []);
+
+  useEffect(() => {
+    const headingElements = Array.from(
+      document.querySelectorAll(
+        '#post-contents > h1, #post-contents > h2, #post-contents > h3',
+      ),
+    );
+    setHeading(headingElements);
   }, []);
 
   return (
-    <S.TocWrapper isSticky={scrollY > tocWrapperTop - 100}>
-      <div>
-        {headings.map((item, idx) => (
-          <S.TocItem
-            key={idx}
-            active={item.id === currentId}
-            ml={
-              item.tagName === 'H1'
-                ? '0'
-                : item.tagName === 'H2'
-                ? '1.2rem'
-                : '2.2rem'
-            }
-            onClick={() => handleClickHeading(item.id)}
-          >
-            {item.innerText}
-          </S.TocItem>
-        ))}
-      </div>
+    <S.TocWrapper>
+      {headings.map((item, idx) => (
+        <S.TocItem
+          key={idx}
+          active={item.id === currentId}
+          ml={
+            item.tagName === 'H1'
+              ? '0.5rem'
+              : item.tagName === 'H2'
+              ? '1.2rem'
+              : '2.2rem'
+          }
+          onClick={() => handleClickHeading(item.id)}
+        >
+          {item.innerText}
+        </S.TocItem>
+      ))}
     </S.TocWrapper>
   );
 };
