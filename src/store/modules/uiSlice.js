537034e634cad91539a68c9bc14500ee8a54d293
KiHoon Roh <started677@gmail.com>
2023-01-31 14:43:02 +0900

    [fix] redux slice 수정
    - 파일명 변경
    - uiSlice 액션함수 수정

--- src/store/modules/uiSlice.js
+++ src/store/modules/uiSlice.js
@@ -1,24 +1,24 @@
 import { createSlice } from '@reduxjs/toolkit';
 
 import {
-  getValueFromLocalStorage,
   setValueToLocalStorage,
+  getValueFromLocalStorage,
 } from '../../utils/localStorage';
 
 const initialState = {
-  darkMode: getValueFromLocalStorage('darkMode') || false,
+  themeMode: getValueFromLocalStorage('themeMode') || 'light',
 };
 
 const uiSlice = createSlice({
   name: 'ui',
   initialState,
   reducers: {
-    toggleMode(state) {
-      state.darkMode = !state.darkMode;
-      setValueToLocalStorage('darkMode', state.darkMode);
+    setTheme(state, action) {
+      state.themeMode = action.payload;
+      setValueToLocalStorage('themeMode', action.payload);
     },
   },
 });
 
-export const { toggleMode } = uiSlice.actions;
+export const { setTheme } = uiSlice.actions;
 export default uiSlice.reducer;
