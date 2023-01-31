e1cf0c18cfb00082d9afae34da41fb40b3193b0e
KiHoon Roh <started677@gmail.com>
2023-01-31 19:48:18 +0900

    [ui] 마크다운 css 수정

--- src/components/Article/Header/index.jsx
+++ src/components/Article/Header/index.jsx
@@ -1,22 +1,26 @@
 import React from 'react';
 import { Link } from 'gatsby';
 
+import TagLabel from '../../TagLabel';
 import Divider from '../../Divider';
 
 // CSS
 import * as S from './style';
 
-const Header = ({ category, title, author, date }) => {
+const Header = ({ category, title, author, date, tags }) => {
   return (
     <S.HeaderWrapper>
-      <S.CategoryLabel>
-        <Link to={`/categories/?category=${category}`}>{category}</Link>
-      </S.CategoryLabel>
+      {category && (
+        <S.CategoryLabel>
+          <Link to={`/categories?q=${category}`}>{category}</Link>
+        </S.CategoryLabel>
+      )}
       <S.Title>{title}</S.Title>
       <S.Information>
         <S.Author> {author} </S.Author>
         <S.Date>· {date} </S.Date>
       </S.Information>
+      {tags && <TagLabel tagList={tags} />}
       <Divider />
     </S.HeaderWrapper>
   );
