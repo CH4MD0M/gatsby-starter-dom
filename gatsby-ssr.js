000ecd001d7e7edf99884ce8a43a704c795e793b
KiHoon Roh <started677@gmail.com>
2023-02-01 23:35:40 +0900

    [chore] gatsby-ssr redux 추가

---
+++ gatsby-ssr.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import { Provider } from 'react-redux';
+import { store } from './src/store';
+
+export const wrapRootElement = ({ element }) => (
+  <Provider store={store}>{element}</Provider>
+);
