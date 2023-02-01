6b0e2aea914078713765ccc88dedecc499b21004
KiHoon Roh <started677@gmail.com>
2023-02-01 23:36:21 +0900

    [chore] blog-config 주소 수정

--- blog-config.js
+++ blog-config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  siteUrl: `https://ch4md0m.blog`,
+  siteUrl: `https://chamdom.blog`,
   title: `gatsby-starter-dom`,
   description: `기록하는 프론트엔드 개발자`,
   utterances: {
@@ -9,7 +9,7 @@ module.exports = {
   author: `Kihoon`,
   name: `노기훈`,
   socials: {
-    email: '', // mailto:d0m__@naver.com
+    email: 'mailto:d0m__@naver.com',
     github: 'https://github.com/',
     instagram: `https://www.instagram.com/`,
   },
