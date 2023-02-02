989fa257ed1deafe578b749fa67aca969a274c74
KiHoon Roh <started677@gmail.com>
2023-02-02 16:38:00 +0900

    [refact] seo 컴포넌트 hooks 적용

--- src/components/Seo/index.js
+++ src/components/Seo/index.js
@@ -1,34 +1,28 @@
 import React from 'react';
 import { Helmet } from 'react-helmet';
-import { useStaticQuery, graphql } from 'gatsby';
+import { useSiteMetaData } from '../../hooks/useSiteMetaData';
 
 const Seo = ({ title, description, url }) => {
-  const { site } = useStaticQuery(seoQuery);
-  const metaDescription = description || site.siteMetadata.description;
-  const metaUrl = url || site.siteMetadata.siteUrl;
+  const data = useSiteMetaData();
+  const { siteMetadata } = data.site;
+
+  const seo = {
+    title: title || siteMetadata.title,
+    description: description || siteMetadata.description,
+    image: `${siteMetadata.siteUrl}/og-image.png`,
+    url: url || siteMetadata.siteUrl,
+  };
 
   return (
     <Helmet>
       <title>{title}</title>
-      <meta property="og:title" content={title} />
-      <meta property="og:url" content={metaUrl} />
-      <meta property="og:description" content={metaDescription} />
-      <meta
-        property="og:image"
-        content={`${site.siteMetadata.siteUrl}/og-image.png`}
-      />
+      <meta property="og:title" content={seo.title} />
+      <meta property="og:url" content={seo.url} />
+      <meta property="og:image" content={seo.image} />
+      <meta property="description" content={seo.description} />
+      <meta property="og:description" content={seo.description} />
     </Helmet>
   );
 };
 
 export default Seo;
-
-const seoQuery = graphql`
-  query {
-    site {
-      siteMetadata {
-        siteUrl
-      }
-    }
-  }
-`;
