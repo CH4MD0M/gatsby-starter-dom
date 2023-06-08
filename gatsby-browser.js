3ebad5a525d6efe83e5ffabdd06fbbccffc94131
KiHoon Roh <started677@gmail.com>
2023-06-08 11:44:38 +0900

    [chore]react-persist 라이브러리 삭제

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -1,18 +1,10 @@
 import React from 'react';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistStore } from 'redux-persist';
 
-import store from './src/store';
-
-let persistor = persistStore(store);
+import { store } from './src/store';
 
 export const wrapRootElement = ({ element }) => (
-  <Provider store={store}>
-    <PersistGate loading={null} persistor={persistor}>
-      {element}
-    </PersistGate>
-  </Provider>
+  <Provider store={store}>{element}</Provider>
 );
 
 const transitionDelay = 100;
