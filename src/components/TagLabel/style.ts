85b128ca4a61f8122931816568f7fc693a6698e2
KiHoon Roh <started677@gmail.com>
2023-11-01 11:47:05 +0900

    [ui]TagItem ui 수정
    태그 글자 길이가 길어지면 줄바꿈을 하고 모바일 화면보다 작아지면 말줄임(ellipsis) 처리함.

--- src/components/TagLabel/style.ts
+++ src/components/TagLabel/style.ts
@@ -1,18 +1,26 @@
 import styled from 'styled-components';
 
 export const TagListWrapper = styled.div`
-  display: flex;
-  gap: 1rem;
-  font-size: 1.5rem;
+  width: 100%;
+  font-size: 1.3rem;
+  letter-spacing: 0.1rem;
 `;
 
 export const TagItem = styled.div`
-  padding: 0.5rem 1.2rem;
-  background: var(--tagBgColor);
+  box-sizing: border-box;
+  display: inline-block;
+  margin: 0.5rem;
+  margin-left: 0;
+  padding: 0.5rem 1rem;
   border-radius: 15px;
-  list-style: none;
   color: var(--linkColor);
+  background: var(--tagBgColor);
+  cursor: pointer;
+  overflow: hidden;
+  text-overflow: ellipsis;
+  max-width: 100%;
   transition: background 0.25s;
+
   &:hover {
     background: var(--hoveredTagBgColor);
   }
