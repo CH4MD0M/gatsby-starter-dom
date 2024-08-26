3f5a3c18f431c646a5e968b71958c7706443caca
KiHoon Roh <started677@gmail.com>
2024-08-26 23:16:14 +0900

    [update]gatsby-plugin-feed 삭제

--- gatsby-config.js
+++ gatsby-config.js
@@ -63,59 +63,6 @@ module.exports = {
       },
     },
     {
-      resolve: 'gatsby-plugin-feed',
-      options: {
-        query: `
-        {
-          site {
-            siteMetadata {
-              title
-              description
-              siteUrl
-              site_url: siteUrl
-            }
-          }
-        }
-        `,
-        feeds: [
-          {
-            serialize: ({ query: { site, allMdx } }) => {
-              return allMdx.edges.map(edge => {
-                return Object.assign({}, edge.node.frontmatter, {
-                  description: edge.node.excerpt,
-                  date: edge.node.frontmatter.date,
-                  url: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  guid: site.siteMetadata.siteUrl + edge.node.fields.slug,
-                  custom_elements: [{ 'content:encoded': edge.node.html }],
-                });
-              });
-            },
-            query: `
-              {
-                allMdx(sort: { fields: frontmatter___date, order: DESC }) {
-                  edges {
-                    node {
-                      excerpt
-                      html
-                      fields {
-                       slug
-                      }
-                      frontmatter {
-                        title
-                        date
-                      }
-                    }
-                  }
-                }
-              }
-            `,
-            output: '/rss.xml',
-            title: `RSS Feed of ${blogConfig.title}`,
-          },
-        ],
-      },
-    },
-    {
       resolve: 'gatsby-plugin-canonical-urls',
       options: {
         siteUrl: `${blogConfig.siteUrl}`,
